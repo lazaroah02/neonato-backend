ce3c4d9e1fe91766e5a76389333853f871cc84ba
lazaroah <lazaroah02@gmail.com>
2023-09-11 10:22:10 -0400

    neonatobackend.pythonanywhere.com added to allowed hosts

--- proyecto_pediatrico/settings.py
+++ proyecto_pediatrico/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4m7razg0v(jhq49@g8$#7vu&0-117eyeh+@ou8env5o)=0qal0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['neonatobackend.pythonanywhere.com']
 
 
 # Application definition
